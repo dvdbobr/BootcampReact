cd284f39f6648e7f532d0fee03f5806531c2cc10
David Bobritsky <dvdbobr@gmail.com>
2021-03-16 15:14:19 +0200

    ex8.3

--- src/day3/Components/BoxColorAndShape.js
+++ src/day3/Components/BoxColorAndShape.js
@@ -21,24 +21,21 @@ export default class BoxColorAndShape extends Component {
             })
             this.setState({ color: `${this.state.r}, ${this.state.g}, ${this.state.b}` })
             this.setState({ counter: this.state.counter + 1 })
-            if (this.state.counter == 5) {
+            if (this.state.counter === 5) {
                 clearInterval(interval)
                 this.setState({ radius: 50 })
             }
         }, 1000);
     }
-    colorChange = () => {
-        this.setState({
-            r: Math.floor((Math.random() * 255)),
-            g: Math.floor((Math.random() * 255)),
-            b: Math.floor((Math.random() * 255))
-        })
-        this.setState({ color: `(${this.state.r}, ${this.state.g}, ${this.state.b})` })
-    }
+
     render() {
         return (
-            <div style={{ width: 400, height: 400, backgroundColor: `rgb(${this.state.color})`, borderRadius: this.state.radius + '%' }}>
-
+            <div style=
+                {{
+                    width: 400, height: 400,
+                    backgroundColor: `rgb(${this.state.color})`,
+                    borderRadius: this.state.radius + '%'
+                }}>
             </div>
         )
     }
